e98712863120c2dffe0ad5a3e979bca5c02c491b
alimiyan <alimiyan123@gmail.com>
2022-01-23 18:00:45 +0530

    extra

---
+++ dictionarysample.py
@@ -0,0 +1,10 @@
+phonebook = {
+    "John" : 92314515577,
+    "William" : 5712492384,
+    "Stephen" : 4329857235,
+    "Arthur" : 349832473289
+}
+
+# Iterate throught the dictionary
+for name, number in phonebook.items():
+    print("Name: {}, Number: {}".format(name,number))
